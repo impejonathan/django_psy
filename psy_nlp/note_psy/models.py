0db99231bae40d78f224073eb59758dd5abce8b6
impejonathan <impe.jonathan@gmail.com>
2023-07-11 14:39:08 +0200

    1er User story ok

--- psy_nlp/note_psy/models.py
+++ psy_nlp/note_psy/models.py
@@ -1,11 +1,11 @@
 # models.py
+
 from django.db import models
 
 class Patient(models.Model):
     id = models.AutoField(primary_key=True)
     patient_lastname = models.CharField(max_length=100)
     patient_firstname = models.CharField(max_length=100)
-    date = models.DateField()
     patient_left = models.BooleanField()
 
     class Meta:
--- psy_nlp/note_psy/views.py
+++ psy_nlp/note_psy/views.py
@@ -3,9 +3,13 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate , logout
 from . import forms
+import pandas as pd
 
 from .models import Patient
 from datetime import date, timedelta
+
+from elasticsearch import Elasticsearch
+es = Elasticsearch(hosts=['http://localhost:9200'])
 
 # Create your views here.
 
@@ -56,13 +60,44 @@ def signup_page(request):
 #     patients = Patient.objects.all()
 #     return render(request, 'note_psy/patient.html', context={'patients': patients})
 
+
+# def patient_page(request):
+#     patients = Patient.objects.filter(patient_left=True).order_by('patient_lastname', 'patient_firstname')
+
+#     return render(request, 'note_psy/patient.html', context={'patients': patients})
+
+
+
+# def patient_page(request):
+#     patients = Patient.objects.filter(patient_left=True).order_by('patient_lastname', 'patient_firstname')
+#     patients_emotions = []
+#     for patient in patients:
+#         res = es.search(index="notes", body={"query": {"match": {"text": patient.patient_lastname}}})
+#         if res['hits']['hits']:
+#             emotion = res['hits']['hits'][0]['_source']['emotion']
+#         else:
+#             emotion = 'Aucune donnée trouvée'
+#         patients_emotions.append((patient, emotion))
+#     return render(request, 'note_psy/patient.html', context={'patients_emotions': patients_emotions})
+
 def patient_page(request):
-    start_date = request.GET.get('start_date')
-    end_date = request.GET.get('end_date')
-
-    patients = Patient.objects.filter(patient_left=True).order_by('patient_lastname', 'patient_firstname', '-date')
-
-    if start_date and end_date:
-        patients = patients.filter(date__range=(start_date, end_date))
-
-    return render(request, 'note_psy/patient.html', context={'patients': patients})
+    patients = Patient.objects.filter(patient_left=True).order_by('patient_lastname', 'patient_firstname')
+    patients_emotions = []
+    for patient in patients:
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"patient_lastname": patient.patient_lastname}},
+                        {"match": {"patient_firstname": patient.patient_firstname}}
+                    ]
+                }
+            }
+        }
+        res = es.search(index='notes', body=query)
+        hits = res['hits']['hits']
+        data = [hit['_source'] for hit in hits]
+        df = pd.DataFrame(data)
+        sentiment_counts = df['emotion'].value_counts(normalize=True)
+        patients_emotions.append((patient, sentiment_counts))
+    return render(request, 'note_psy/patient.html', context={'patients_emotions': patients_emotions})
